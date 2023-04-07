84eb64f2ac02cc8c9697fcacd41b59e9b2cc18c5
hanc94 <116841259+hanc94@users.noreply.github.com>
2023-04-07 10:14:53 -0500

    Add files via upload

---
+++ iot/src/TestFireBase.java
@@ -0,0 +1,32 @@
+
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class TestFireBase {
+
+
+    public static void main(String[] args) {
+        //Project name
+        String project="changua";
+
+        //Secret
+        String secret="iaGwhq1kLmIm2fwKQIOBw8Px3JT00LSIRoxQlBJN";
+
+        //New Firebase object
+        Firebase firebase=new Firebase(project,secret);
+
+        //Name of document to update
+        String document="doubleint";
+
+        //Data for document
+        String str = "{     \"output\": 0.377,\"control\": -1.259  }";
+
+        //Writes document to Firebase
+        System.out.println("Firebase response: "+firebase.write(document,str));
+
+        //Reads document from Firebase
+        System.out.println(firebase.read("doubleint"));
+    }
+}
